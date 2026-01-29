a201a74e5b003f3a93d2318dea507659c8ceea85
Jiahe Wang <2794099410@qq.com>
2026-01-29 19:11:34 +0800

    upload

--- inversion/models/classifiers/resnet.py
+++ inversion/models/classifiers/resnet.py
@@ -9,31 +9,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-"""
-class BasicBlock(nn.Module):
-    expansion = 1
- 
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
- 
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
- 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-"""
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -41,7 +16,7 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.relu1 = nn.ReLU(inplace=True)  # <--- 新增: 第一个ReLU模块
+        self.relu1 = nn.ReLU(inplace=True)  
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
@@ -51,10 +26,10 @@ class BasicBlock(nn.Module):
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
             )
-        self.relu2 = nn.ReLU(inplace=True)  # <--- 新增: 第二个ReLU模块 (在残差连接后)
+        self.relu2 = nn.ReLU(inplace=True)  
 
     def forward(self, x):
-        identity = x # 保存原始输入以用于 shortcut
+        identity = x  
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -93,46 +68,6 @@ class Bottleneck(nn.Module):
         out += self.shortcut(x)
         out = F.relu(out)
         return out
- 
-"""
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
- 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
- 
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
- 
-    def forward(self, x, return_features=False):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        out = F.relu(x)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.adaptive_avg_pool2d(out, (1,1))
-        feature = out.view(out.size(0), -1)
-        out = self.linear(feature)
-
-        if return_features:
-            return out, feature
-        else:
-            return out
-"""
 
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
@@ -141,26 +76,25 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)  # <--- 新增：定义ReLU模块
+        self.relu = nn.ReLU(inplace=True)  
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride_val=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride_val=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride_val=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride_val=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride_val): # Renamed 'stride' to 'stride_val'
+    def _make_layer(self, block, planes, num_blocks, stride_val):
         strides = [stride_val] + [1]*(num_blocks-1)
         layers = []
-        for current_stride in strides: # Renamed 'stride' to 'current_stride'
+        for current_stride in strides: 
             layers.append(block(self.in_planes, planes, current_stride))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x, return_features=False):
-        x_conv1 = self.conv1(x) # Keep original variable name if preferred
+        x_conv1 = self.conv1(x) 
         x_bn1 = self.bn1(x_conv1)
-        out = self.relu(x_bn1)  # <--- 修改：使用定义的 self.relu 模块
-        # ... (rest of the forward pass remains the same)
+        out = self.relu(x_bn1)  
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
